cbf1f601de66d07e6c5bc0fca9c95fbd07392364
Johanna Skinnari <jskinnari@gmail.com>
2021-10-18 15:16:28 +0200

    #43 make modal flow

--- screens/HouseHoldScreens/ChoresScreen.tsx
+++ screens/HouseHoldScreens/ChoresScreen.tsx
@@ -1,7 +1,7 @@
 import { CompositeScreenProps, useTheme } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
-import { Button } from "react-native-paper";
+import { Button, Modal } from "react-native-paper";
 import { HouseholdStackScreenProps } from "../../navigation/HouseholdNavigator";
 import { ProfileStackScreenProps } from "../../navigation/ProfileNavigator";
 
@@ -11,12 +11,53 @@ type Props = CompositeScreenProps<
 >;
 
 export default function ChoresScreen({navigation}: Props) {
+  const [openAdd, setOpenAdd] = useState(false);
+  const [openChore, setOpenChore] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
   const { colors } = useTheme();
+  
   return (
-    <View>
-      <Text style={{ color: colors.text }}>Hello from ChoresScreen</Text>
-      {/* använd custom component för knapp*/}
-      <Button onPress={() => navigation.navigate("Profile")}>Profile</Button>
-    </View>
+    <>
+      <View>
+        <Text style={{ color: colors.text }}>Hello from ChoresScreen</Text>
+        {/* använd custom component för knapp*/}
+        <Button onPress={() => navigation.navigate("Profile")}>Profil</Button>
+
+        {/* 2 modaler, skapa ny syssla och visa information för vald syssla */}
+          {/* provisorisk knapp istället för syssla */}
+        <Button onPress={() => setOpenChore(true)}>Provisorisk syssla</Button>
+        {/* använd custom component för knapp*/}
+        <Button onPress={() => setOpenAdd(true)}>Lägg till Syssla</Button>
+      </View>
+      {openAdd && (
+        <Modal 
+          visible={openAdd}
+          onDismiss={() => setOpenAdd(false)}
+        >
+          <Text style={{ color: colors.text }}>Exampel Modal för lägga till syssla.  Click outside this area to dismiss.</Text>
+          
+        </Modal>
+      )}
+      {openChore && (
+        <>
+        <Modal 
+          visible={openChore}
+          onDismiss={() => setOpenChore(false)}
+        >
+          <Text style={{ color: colors.text }}>Exampel Modal med information för vald syssla.  Click outside this area to dismiss.</Text>
+          {/* använd custom component för knapp*/}
+          <Button onPress={() => setOpenEdit(true)}>Ändra</Button>
+        </Modal>
+        {openEdit && (
+          <Modal 
+            visible={openChore}
+            onDismiss={() => setOpenEdit(false)}
+          >
+            <Text style={{ color: colors.text }}>Exampel Modal för att ädra på vald syssla.  Click outside this area to dismiss.</Text>
+          </Modal>
+        )}
+        </>
+      )}
+    </>
   );
 }
--- screens/ProfileScreens/JoinHouseHoldScreen.tsx
+++ screens/ProfileScreens/JoinHouseHoldScreen.tsx
@@ -1,16 +1,30 @@
 import { useTheme } from "@react-navigation/native";
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Modal } from "react-native";
 import { Button } from "react-native-paper";
 import { ProfileStackScreenProps } from "../../navigation/ProfileNavigator";
 
 export default function JoinHouseholdScreen({navigation}: ProfileStackScreenProps<"JoinHousehold">) {
   const { colors } = useTheme();
+  const [openAvatarPicker, setOpenAvatarPicker] = useState(false);
   return (
+    <>
     <View>
       <Text style={{ color: colors.text }}>Hello from JoinHouseholdScreen</Text>
       {/* använd custom component för knapp*/}
-      <Button onPress={() => navigation.navigate("Profile")}>Gå med i hushåll/ profil</Button>
+      <Button onPress={() => setOpenAvatarPicker(true)}>Gå med i hushåll/ modal</Button>
+      {/* 1 modal för välja avatar */}
     </View>
+    {openAvatarPicker && (
+      <Modal 
+        visible={openAvatarPicker}
+        onDismiss={() => setOpenAvatarPicker(false)}
+      >
+        <Text style={{ color: colors.text }}>Exempel Modal som visar modal där man väljer Avatar.  Click outside this area to dismiss.</Text>
+        {/* använd custom component för knapp*/}
+        <Button onPress={() => navigation.navigate("Profile")}>Spara/ profil</Button>
+      </Modal>
+    )}
+    </>
   );
 }
