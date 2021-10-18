a91e6d6ecad1b04a85a9b1e6adb0dca30ae142a8
Johanna Skinnari <jskinnari@gmail.com>
2021-10-18 16:56:54 +0200

    #43 Add modal for Delete. Add functonality for going back from modals

--- screens/HouseHoldScreens/ChoresScreen.tsx
+++ screens/HouseHoldScreens/ChoresScreen.tsx
@@ -14,6 +14,7 @@ export default function ChoresScreen({navigation}: Props) {
   const [openAdd, setOpenAdd] = useState(false);
   const [openChore, setOpenChore] = useState(false);
   const [openEdit, setOpenEdit] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
   const { colors } = useTheme();
   
   return (
@@ -35,7 +36,8 @@ export default function ChoresScreen({navigation}: Props) {
           onDismiss={() => setOpenAdd(false)}
         >
           <Text style={{ color: colors.text }}>Exampel Modal för lägga till syssla.  Click outside this area to dismiss.</Text>
-          
+          <Button onPress={() => setOpenAdd(false)}>Spara</Button>
+          <Button onPress={() => setOpenAdd(false)}>Stäng</Button>
         </Modal>
       )}
       {openChore && (
@@ -47,14 +49,31 @@ export default function ChoresScreen({navigation}: Props) {
           <Text style={{ color: colors.text }}>Exampel Modal med information för vald syssla.  Click outside this area to dismiss.</Text>
           {/* använd custom component för knapp*/}
           <Button onPress={() => setOpenEdit(true)}>Ändra</Button>
+          <Button onPress={() => setOpenChore(false)}>Klar</Button>
+          <Button onPress={() => setOpenChore(false)}>Stäng</Button>
         </Modal>
         {openEdit && (
+          <>
           <Modal 
             visible={openChore}
             onDismiss={() => setOpenEdit(false)}
           >
             <Text style={{ color: colors.text }}>Exampel Modal för att ädra på vald syssla.  Click outside this area to dismiss.</Text>
+            <Button onPress={() => {setOpenEdit(false); setOpenChore(false)}}>Spara</Button>
+            <Button onPress={() => setOpenEdit(false)}>Stäng</Button>
+            <Button onPress={() => setOpenDelete(true)}>Ta bort</Button>
           </Modal>
+          {openDelete && (
+            <Modal 
+              visible={openChore}
+              onDismiss={() => setOpenDelete(false)}
+            >
+              <Text style={{ color: colors.text }}>Exampel Modal för att Tabort vald syssla.  Click outside this area to dismiss.</Text>
+              <Button onPress={() => {setOpenDelete(false); setOpenEdit(false); setOpenChore(false)}}>Ta bort</Button>
+              <Button onPress={() => setOpenDelete(false)}>Stäng</Button>
+            </Modal>
+          )}
+          </>
         )}
         </>
       )}
