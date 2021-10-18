5228f95bc634c0b7ac939bd0bb40f1b326dc5ff0
JonathanMarkert <jonathan@markert.se>
2021-10-18 21:16:44 +0200

    #45-Add-safeAreaProvider-in-app-and-safeAreaView-in-signin-signupscreens

--- screens/HouseHoldScreens/ChoresScreen.tsx
+++ screens/HouseHoldScreens/ChoresScreen.tsx
@@ -10,13 +10,13 @@ type Props = CompositeScreenProps<
   ProfileStackScreenProps<"Profile">
 >;
 
-export default function ChoresScreen({navigation}: Props) {
+export default function ChoresScreen({ navigation }: Props) {
   const [openAdd, setOpenAdd] = useState(false);
   const [openChore, setOpenChore] = useState(false);
   const [openEdit, setOpenEdit] = useState(false);
   const [openDelete, setOpenDelete] = useState(false);
   const { colors } = useTheme();
-  
+
   return (
     <>
       <View>
@@ -25,56 +25,74 @@ export default function ChoresScreen({navigation}: Props) {
         <Button onPress={() => navigation.navigate("Profile")}>Profil</Button>
 
         {/* 2 modaler, skapa ny syssla och visa information för vald syssla */}
-          {/* provisorisk knapp istället för syssla */}
+        {/* provisorisk knapp istället för syssla */}
         <Button onPress={() => setOpenChore(true)}>Provisorisk syssla</Button>
         {/* använd custom component för knapp*/}
         <Button onPress={() => setOpenAdd(true)}>Lägg till Syssla</Button>
       </View>
       {openAdd && (
-        <Modal 
-          visible={openAdd}
-          onDismiss={() => setOpenAdd(false)}
-        >
-          <Text style={{ color: colors.text }}>Exampel Modal för lägga till syssla.  Click outside this area to dismiss.</Text>
+        <Modal visible={openAdd} onDismiss={() => setOpenAdd(false)}>
+          <Text style={{ color: colors.text }}>
+            Exampel Modal för lägga till syssla. Click outside this area to
+            dismiss.
+          </Text>
           <Button onPress={() => setOpenAdd(false)}>Spara</Button>
           <Button onPress={() => setOpenAdd(false)}>Stäng</Button>
         </Modal>
       )}
       {openChore && (
         <>
-        <Modal 
-          visible={openChore}
-          onDismiss={() => setOpenChore(false)}
-        >
-          <Text style={{ color: colors.text }}>Exampel Modal med information för vald syssla.  Click outside this area to dismiss.</Text>
-          {/* använd custom component för knapp*/}
-          <Button onPress={() => setOpenEdit(true)}>Ändra</Button>
-          <Button onPress={() => setOpenChore(false)}>Klar</Button>
-          <Button onPress={() => setOpenChore(false)}>Stäng</Button>
-        </Modal>
-        {openEdit && (
-          <>
-          <Modal 
-            visible={openChore}
-            onDismiss={() => setOpenEdit(false)}
-          >
-            <Text style={{ color: colors.text }}>Exampel Modal för att ädra på vald syssla.  Click outside this area to dismiss.</Text>
-            <Button onPress={() => {setOpenEdit(false); setOpenChore(false)}}>Spara</Button>
-            <Button onPress={() => setOpenEdit(false)}>Stäng</Button>
-            <Button onPress={() => setOpenDelete(true)}>Ta bort</Button>
+          <Modal visible={openChore} onDismiss={() => setOpenChore(false)}>
+            <Text style={{ color: colors.text }}>
+              Exampel Modal med information för vald syssla. Click outside this
+              area to dismiss.
+            </Text>
+            {/* använd custom component för knapp*/}
+            <Button onPress={() => setOpenEdit(true)}>Ändra</Button>
+            <Button onPress={() => setOpenChore(false)}>Klar</Button>
+            <Button onPress={() => setOpenChore(false)}>Stäng</Button>
           </Modal>
-          {openDelete && (
-            <Modal 
-              visible={openChore}
-              onDismiss={() => setOpenDelete(false)}
-            >
-              <Text style={{ color: colors.text }}>Exampel Modal för att Tabort vald syssla.  Click outside this area to dismiss.</Text>
-              <Button onPress={() => {setOpenDelete(false); setOpenEdit(false); setOpenChore(false)}}>Ta bort</Button>
-              <Button onPress={() => setOpenDelete(false)}>Stäng</Button>
-            </Modal>
+          {openEdit && (
+            <>
+              <Modal visible={openChore} onDismiss={() => setOpenEdit(false)}>
+                <Text style={{ color: colors.text }}>
+                  Exampel Modal för att ädra på vald syssla. Click outside this
+                  area to dismiss.
+                </Text>
+                <Button
+                  onPress={() => {
+                    setOpenEdit(false);
+                    setOpenChore(false);
+                  }}
+                >
+                  Spara
+                </Button>
+                <Button onPress={() => setOpenEdit(false)}>Stäng</Button>
+                <Button onPress={() => setOpenDelete(true)}>Ta bort</Button>
+              </Modal>
+              {openDelete && (
+                <Modal
+                  visible={openChore}
+                  onDismiss={() => setOpenDelete(false)}
+                >
+                  <Text style={{ color: colors.text }}>
+                    Exampel Modal för att Tabort vald syssla. Click outside this
+                    area to dismiss.
+                  </Text>
+                  <Button
+                    onPress={() => {
+                      setOpenDelete(false);
+                      setOpenEdit(false);
+                      setOpenChore(false);
+                    }}
+                  >
+                    Ta bort
+                  </Button>
+                  <Button onPress={() => setOpenDelete(false)}>Stäng</Button>
+                </Modal>
+              )}
+            </>
           )}
-          </>
-        )}
         </>
       )}
     </>
