63bbd33662c56d5f361dbbc61a03c368f50e13e4
EnesMas <70940688+EnesMas@users.noreply.github.com>
2021-10-19 06:18:23 +0200

    Initialized firebase

--- App.tsx
+++ App.tsx
@@ -5,10 +5,16 @@ import { StyleSheet } from "react-native";
 import { AppearanceProvider, useColorScheme } from "react-native-appearance";
 import { Provider as PaperProvider } from "react-native-paper";
 import { getTheme } from "./components/common/Theme";
+import ApiKeys from './database/ApiKeys';
+import firebase from 'firebase/app'
 
 import RootNavigation from "./navigation/RootNavigation";
 
 export default function App() {
+  
+  // Initialize firebase...
+  firebase.initializeApp(ApiKeys.FirebaseConfig);
+
   const scheme = useColorScheme();
   const isDarkTheme = scheme === "dark";
   const theme = getTheme(isDarkTheme);
