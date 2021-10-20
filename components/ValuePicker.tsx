41d5fdabcd597e2f3c1a28b6bdffc002f44ef92b
Johanna Skinnari <jskinnari@gmail.com>
2021-10-20 23:54:06 +0200

    #7 make ValuePicker

---
+++ components/ValuePicker.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { Pressable, View, Text, StyleSheet } from "react-native";
+import { useTheme } from "react-native-paper";
+
+interface Props {
+  selectValue: (value: number) => void;
+}
+
+export default function ValuePicker({ selectValue }: Props) {
+  const { colors } = useTheme();
+  return (
+    <View
+      style={[styles.input, styles.value, {backgroundColor: colors.surface}]}
+    >
+      <View style={[styles.valueContainer, {borderLeftColor: colors.darkPink, borderRightColor: colors.darkPink}]}>
+        <View style={[styles.valueInnerContainer, {borderRightColor: colors.darkPink}]}>
+          <Pressable 
+            style={[styles.valueCircle, styles.marginRight5, {backgroundColor: colors.valueOne}]} 
+            onPress={() => selectValue(1)}
+          >
+            <Text style={[{color: colors.text}]}>
+              1
+            </Text>
+          </Pressable>
+        </View>
+        <View style={[styles.valueInnerContainer, {borderRightColor: colors.darkPink}]}>
+          <Pressable 
+              style={[styles.valueCircle, styles.marginRight5, {backgroundColor: colors.valueTwo}]}
+            onPress={() => selectValue(2)}
+          >
+            <Text style={[{color: colors.text}]}>
+              2
+            </Text>
+          </Pressable>
+        </View>
+        <View style={[styles.valueInnerContainer, {borderRightColor: colors.darkPink}]}>
+          <Pressable 
+              style={[styles.valueCircle, styles.marginRight5, {backgroundColor: colors.valueFour}]} 
+            onPress={() => selectValue(4)}
+          >
+            <Text style={[{color: colors.text}]}>
+              4
+            </Text>
+          </Pressable>
+        </View>
+        <View style={[styles.valueInnerContainer, {borderRightColor: colors.darkPink}]}>
+          <Pressable 
+              style={[styles.valueCircle, styles.marginRight5, {backgroundColor: colors.valueSix}]}  
+            onPress={() => selectValue(6)}
+          >
+            <Text style={[{color: colors.text}]}>
+              6
+            </Text>
+          </Pressable>
+        </View>
+        <View style={styles.valueLastInnerContainer}>
+          <Pressable 
+              style={[styles.valueCircle, {backgroundColor: colors.valueEight}]}
+            onPress={() => selectValue(8)}
+          >
+            <Text style={[{color: colors.text}]}>
+              8
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    width: "100%",
+    marginTop: 15,
+    borderBottomWidth: 0,
+    fontSize: 18,
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+  },
+  value: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 80,
+  },
+  valueContainer: {
+    borderLeftWidth: 0.5, 
+    flexDirection: "row", 
+    justifyContent: "space-evenly", 
+    width: "100%", 
+    // borderRightColor: "red", 
+    borderRightWidth: 0.5,
+    padding: 0,
+    height: "100%"
+  },
+  valueInnerContainer: {
+    borderRightWidth: 0.5, 
+    justifyContent: "center", 
+    alignItems: "center",
+  },
+  valueLastInnerContainer: {
+    justifyContent: "center", 
+    alignItems: "center",
+  },
+  valueCircle: {
+    borderRadius: 50, 
+    width: 48, 
+    height: 48, 
+    // backgroundColor: colors.disabled, 
+    justifyContent: "center", 
+    alignItems: "center",
+  },
+  marginRight5: {
+    marginRight: 5,
+  },
+})
--- components/modals/adminChoreModal.tsx
+++ components/modals/adminChoreModal.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Pressable, StyleSheet, TextInput, Text, View } from "react-native";
 import { useStore } from "react-redux";
 import { color } from "react-native-reanimated";
+import ValuePicker from "../ValuePicker";
 
 interface Props {
   onSave: () => void;
@@ -24,7 +25,7 @@ export default function AdminChoreModal({ onSave, onClose}: Props) {
 
   const selectValue = (value: number) => {
     setValue(value);
-    // console.log(value);
+    console.log(value);
     switch (value) {
       case 1:
         setColorValue(colors.valueOne)
@@ -52,17 +53,6 @@ export default function AdminChoreModal({ onSave, onClose}: Props) {
     <Card style={styles.card}>
       <Card.Title title="Skapa en ny syssla" style={styles.cardTitle}/>
       <Card.Content style={[styles.cardContent, {backgroundColor: colors.background}]}>
-        {/* <Title>Card title</Title>' */}
-        {/* <TextInput
-          mode="flat"
-          // outlineColor={colors.surface}
-          style={[styles.textInput, {backgroundColor: colors.surface,}]}
-          underlineColor="#FFFFFF100"
-          placeholder="Titel"
-          // label="Syssla"
-          value={name}
-          onChangeText={(name: string) => setName(name)}
-        /> */}
         <TextInput
           style={[styles.input, styles.textInput, {backgroundColor: colors.surface, color: colors.onSurface}]}
           placeholder="Titel"
@@ -126,62 +116,7 @@ export default function AdminChoreModal({ onSave, onClose}: Props) {
             </Text>
           </View>
         </Pressable>) :
-        (<View
-          style={[styles.input, styles.value, {backgroundColor: colors.surface}]}
-        >
-          <View style={[styles.valueContainer, {borderLeftColor: colors.darkPink, borderRightColor: colors.darkPink}]}>
-            <View style={[styles.valueInnerContainer, {borderRightColor: colors.darkPink}]}>
-              <Pressable 
-                style={[styles.valueCircle, styles.marginRight5, {backgroundColor: colors.valueOne}]} 
-                onPress={() => selectValue(1)}
-              >
-                <Text style={[{color: colors.text}]}>
-                  1
-                </Text>
-              </Pressable>
-            </View>
-            <View style={[styles.valueInnerContainer, {borderRightColor: colors.darkPink}]}>
-              <Pressable 
-                 style={[styles.valueCircle, styles.marginRight5, {backgroundColor: colors.valueTwo}]}
-                onPress={() => selectValue(2)}
-              >
-                <Text style={[{color: colors.text}]}>
-                  2
-                </Text>
-              </Pressable>
-            </View>
-            <View style={[styles.valueInnerContainer, {borderRightColor: colors.darkPink}]}>
-              <Pressable 
-                 style={[styles.valueCircle, styles.marginRight5, {backgroundColor: colors.valueFour}]} 
-                onPress={() => selectValue(4)}
-              >
-                <Text style={[{color: colors.text}]}>
-                  4
-                </Text>
-              </Pressable>
-            </View>
-            <View style={[styles.valueInnerContainer, {borderRightColor: colors.darkPink}]}>
-              <Pressable 
-                 style={[styles.valueCircle, styles.marginRight5, {backgroundColor: colors.valueSix}]}  
-                onPress={() => selectValue(6)}
-              >
-                <Text style={[{color: colors.text}]}>
-                  6
-                </Text>
-              </Pressable>
-            </View>
-            <View style={styles.valueLastInnerContainer}>
-              <Pressable 
-                 style={[styles.valueCircle, {backgroundColor: colors.valueEight}]}
-                onPress={() => selectValue(8)}
-              >
-                <Text style={[{color: colors.text}]}>
-                  8
-                </Text>
-              </Pressable>
-            </View>
-          </View>
-        </View>)}
+        (<ValuePicker selectValue={selectValue} />)}
       </Card.Content>
       <Card.Actions style={styles.cardAction}>
         <Button icon={"plus-circle-outline"} color={colors.text} onPress={onSave}>Spara</Button>
@@ -234,11 +169,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.27,
     shadowRadius: 4.65,
     elevation: 6,
-
-    // flex: 0,
-    // order: 0,
-    // alignSelf: "stretch",
-    // flexGrow: 0,
   },
   textInput: {
     alignItems: "flex-start",
@@ -272,36 +202,36 @@ const styles = StyleSheet.create({
   subText: {
     fontSize: 12,
   },
-  valueContainer: {
-    borderLeftWidth: 0.5, 
-    flexDirection: "row", 
-    justifyContent: "space-evenly", 
-    width: "100%", 
-    // borderRightColor: "red", 
-    borderRightWidth: 0.5,
-    padding: 0,
-    height: "100%"
-  },
-  valueInnerContainer: {
-    borderRightWidth: 0.5, 
-    justifyContent: "center", 
-    alignItems: "center",
-  },
-  valueLastInnerContainer: {
-    justifyContent: "center", 
-    alignItems: "center",
-  },
-  valueCircle: {
-    borderRadius: 50, 
-    width: 48, 
-    height: 48, 
-    // backgroundColor: colors.disabled, 
-    justifyContent: "center", 
-    alignItems: "center",
-  },
-  marginRight5: {
-    marginRight: 5,
-  },
+  // valueContainer: {
+  //   borderLeftWidth: 0.5, 
+  //   flexDirection: "row", 
+  //   justifyContent: "space-evenly", 
+  //   width: "100%", 
+  //   // borderRightColor: "red", 
+  //   borderRightWidth: 0.5,
+  //   padding: 0,
+  //   height: "100%"
+  // },
+  // valueInnerContainer: {
+  //   borderRightWidth: 0.5, 
+  //   justifyContent: "center", 
+  //   alignItems: "center",
+  // },
+  // valueLastInnerContainer: {
+  //   justifyContent: "center", 
+  //   alignItems: "center",
+  // },
+  // valueCircle: {
+  //   borderRadius: 50, 
+  //   width: 48, 
+  //   height: 48, 
+  //   // backgroundColor: colors.disabled, 
+  //   justifyContent: "center", 
+  //   alignItems: "center",
+  // },
+  // marginRight5: {
+  //   marginRight: 5,
+  // },
   litleCircle: {
     borderRadius: 50, 
     width: 25, 
