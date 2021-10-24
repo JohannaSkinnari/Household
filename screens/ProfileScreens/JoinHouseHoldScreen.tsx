fc7c349cca9de4af31689a8204369946ddf60d01
Alpha-85 <ljungdahlt@gmail.com>
2021-10-24 19:42:44 +0200

    #3 Added Snackbar & added a useEffect

--- screens/ProfileScreens/JoinHouseHoldScreen.tsx
+++ screens/ProfileScreens/JoinHouseHoldScreen.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { Alert, Image, StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, View, Text } from "react-native";
+import { Snackbar, useTheme } from "react-native-paper";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 import JoinHouseHoldForm from "../../components/JoinHouseHoldForm";
 import { ProfileStackScreenProps } from "../../navigation/ProfileNavigator";
 import { getHouseholdCodes } from "../../redux/houseHold/houseHoldSelector";
@@ -9,6 +11,9 @@ export default function JoinHouseholdScreen({
   navigation,
 }: ProfileStackScreenProps<"JoinHousehold">) {
   const households = useAppSelector(getHouseholdCodes);
+  const { colors } = useTheme();
+  const [error, setError] = useState(false);
+  const [showSnackbar, setShowSnackbar] = useState<boolean>(false);
 
   function checkCode(code: number) {
     const householdCode: boolean = households.some(
@@ -17,12 +22,22 @@ export default function JoinHouseholdScreen({
     const house = households.find((h) => (h.id, h.houseHoldCode == code));
 
     if (!householdCode) {
-      // TODO should be a snackbar instead
-      return Alert.alert("Achtung!", "scheiße auch nichts gefunden");
+      setError(true);
     }
     if (householdCode && house) {
+      setError(false);
       navigation.navigate("HouseholdInfo", { id: house.id });
     }
+  }
+  useEffect(() => {
+    if (error == true) {
+      setShowSnackbar(true);
+      setError(!error);
+    }
+  }, [checkCode]);
+
+  function onDismissSnackBar() {
+    setShowSnackbar(false);
   }
 
   return (
@@ -38,6 +53,23 @@ export default function JoinHouseholdScreen({
       </View>
       <View style={{ flex: 1 }}>
         <JoinHouseHoldForm onSubmitSuccess={checkCode} />
+        <View>
+          <Snackbar
+            style={{
+              backgroundColor: colors.darkPink,
+            }}
+            visible={showSnackbar}
+            duration={3000}
+            onDismiss={onDismissSnackBar}
+            action={{
+              icon: "close-circle-outline",
+              label: "stäng",
+              onPress: () => setShowSnackbar(false),
+            }}
+          >
+            <Text>Inget hushåll hittades </Text>
+          </Snackbar>
+        </View>
       </View>
     </View>
   );
