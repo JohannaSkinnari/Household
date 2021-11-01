b4bcd89a5c0aa78f6bc2f90930fbd09a2d470c7c
JonathanMarkert <jonathan@markert.se>
2021-11-01 20:13:07 +0100

    trying to fix statistics JS, DS, EM, JM
    Co-authored-by: Daniel Sundin <DanielSundin@users.noreply.github.com>

--- assets/AvatarData/data.ts
+++ assets/AvatarData/data.ts
@@ -8,3 +8,14 @@ export const avatars = [
   { icon: require("../images/pig.png"), id: 7 },
   { icon: require("../images/unicorn.png"), id: 8 },
 ];
+
+export const avatarColors = [
+  { color: "colors.yellow", id: 1 },
+  { color: "colors.blue", id: 2 },
+  { color: "colors.orange", id: 3 },
+  { color: "colors.green", id: 4 },
+  { color: "colors.darkPink", id: 5 },
+  { color: "colors.brown", id: 6 },
+  { color: "colors.pink", id: 7 },
+  { color: "colors.purple", id: 8 },
+];
