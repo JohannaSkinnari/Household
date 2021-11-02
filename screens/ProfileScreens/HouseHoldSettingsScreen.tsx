e01170867436f4aa17dda0550aee1aae4eae1c30
Daniel <79dansun@skola.boras.se>
2021-11-02 16:01:27 +0100

    #17-edit-button-now-saves-to-db-and-state
    Co-authored-by: Jonathan Markert <JonathanMarkert@users.noreply.github.com>
    Co-authored-by: JohannaSkinnari <jskinnari@gmail.com>
    Co-authored-by: Thomas<ljungdahlt@gmail.com>

--- screens/ProfileScreens/HouseHoldSettingsScreen.tsx
+++ screens/ProfileScreens/HouseHoldSettingsScreen.tsx
@@ -1,14 +1,16 @@
 import { useTheme } from "react-native-paper";
 import React from "react";
-import { Text, View } from "react-native";
-import CustomButton from "../../components/common/CustomButton";
+import { View, Text, StyleSheet } from "react-native";
 import EditHouseHoldForm from "../../components/EditHouseHoldForm";
 import { ProfileStackScreenProps } from "../../navigation/ProfileNavigator";
 import { useAppSelector } from "../../redux/reduxHooks";
 
 export default function HouseholdSettingsScreen({
   navigation,
+  route,
 }: ProfileStackScreenProps<"HouseholdSettings">) {
+  const houseId = route.params.id;
+
   const households = useAppSelector(s => s.houseHoldList);
   const { colors } = useTheme();
   const toggleSuccess = () => {
@@ -18,14 +20,32 @@ export default function HouseholdSettingsScreen({
   };
 
   return (
-    <View>
-      <View>
-        {/* <EditHouseHoldForm onSubmitSuccess={toggleSuccess}/>        */}
+    <View style={styles.root}>
+      <View style={styles.formContainer}>
+        <Text style={[styles.text, { color: colors.text }]}>
+          Ändra hushållets namn:
+        </Text>
+        <EditHouseHoldForm onSubmitSuccess={toggleSuccess} houseId={houseId} />
       </View>
-      <Text>
-        Knapp för att ändra hushållsnamn och här ser man också requests att gå
-        med
-      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: "bold",
+    alignItems: "center",
+  },
+  formContainer: {
+    flex: 4,
+    marginTop: 14,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
